61c7e82b755adc7b9407c84005df227a9ad95f7d
Matthew Flickinger <mflick@umich.edu>
2016-01-03 22:00:37 -0500

    Allow login via Facebook and Google via Passport

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 /* global require */
 /* eslint-disable no-unused-vars */
 
-var express = require("express");
+var express = require("express"),
+	session = require("express-session"),
+	passport = require("passport"),
+	FacebookStrategy = require("passport-facebook").Strategy,
+	GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
 var app = express();
 
 function promiseWrap(x) {
@@ -15,9 +20,101 @@ var config = require("/Users/matthew/.greenroom.config");
 
 app.set("views", "./views");
 app.set("view engine", "jade");
+app.use(session({secret: config.sessionSecret,
+	resave: false,
+	saveUninitialized : false
+}));
+
+passport.use(new FacebookStrategy({
+	clientID: config.facebook.clientID,
+	clientSecret: config.facebook.clientSecret,
+	callbackURL: config.facebook.callbackURL
+}, function(accessToken, refreshToken, profile, done) {
+	console.log(accessToken, refreshToken, profile);
+	return done(null, profile);
+}));
+passport.use(new GoogleStrategy({
+	clientID: config.google.clientID,
+	clientSecret: config.google.clientSecret,
+	callbackURL: config.google.callbackURL
+}, function(accessToken, refreshToken, profile, done) {
+	console.log(accessToken, refreshToken, profile);
+	return done(null, profile);
+}));
+GoogleStrategy.prototype.userProfile = function(accessToken, done) {
+	this._oauth2.get("https://www.googleapis.com/plus/v1/people/me/openIdConnect", accessToken, function(err, body, res) {
+		if (err) {
+			return done(err);
+		}
+
+		try {
+			var json = JSON.parse(body),
+				i, len;
+
+			var profile = {
+				provider: "google"
+			};
+			profile.id = json.id;
+			profile.displayName = json.displayName;
+			if (json.name) {
+				profile.name = {
+					familyName: json.name.familyName,
+					givenName: json.name.givenName
+				};
+			}
+			if (json.emails) {
+				profile.emails = [];
+				for (i = 0, len = json.emails.length; i < len; ++i) {
+					profile.emails.push({
+						value: json.emails[i].value,
+						type: json.emails[i].type
+					});
+				}
+			}
+			if (json.image) {
+				profile.photos = [{
+					value: json.image.url
+				}];
+			}
+			profile.gender = json.gender;
+
+			profile._raw = body;
+			profile._json = json;
+
+			done(null, profile);
+		} catch (e) {
+			done(e);
+		}
+	});
+};
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook/callback", passport.authenticate("facebook", {
+	successRedirect: "/show",
+	failureRedirect: "/login"
+}), function(req, res) {
+	res.redirect("/");
+});
+
+app.get("/auth/google", passport.authenticate("google", { scope: ["openid","profile","email"] }));
+app.get("/auth/google/callback", passport.authenticate("google", {
+	successRedirect: "/show",
+	failureRedirect: "/login"
+}), function(req, res) {
+	res.redirect("/");
+});
 
 app.get("/", function(req, res) {
-	res.render("layout");
+	res.render("login");
 });
 
 app.get("/gcaltest", function(req, res) {
